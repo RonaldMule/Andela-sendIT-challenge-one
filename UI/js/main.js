4fa3cef4494c53800ef5a1d708c0d8b94152abec
anyatibrian <anyatibrian@gmail.com>
2018-11-01 16:48:58 +0300

    adds css and js to the update delivery status [finishes #161648413]

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,8 +1,14 @@
 var  parcel_order_modal = document.getElementById('order-parcel-modal');
+var update_parcel_Destination_modal= document.getElementById('update-parcel-modal');
 function display_add_parcel_order() {
     parcel_order_modal.style.display = "block";
 }
 /* function to close the order parcel modal*/
 function close_modal() {
     parcel_order_modal.style.display ="none";
+    update_parcel_Destination_modal.style.display="none";
+}
+/* function update Delivery Modal */
+function update_parcel_order() {
+    update_parcel_Destination_modal.style.display="block";
 }
