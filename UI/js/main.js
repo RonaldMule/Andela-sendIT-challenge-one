59ca379cdcabfeb5ce7fea8647cb91db577a8a21
anyatibrian <anyatibrian@gmail.com>
2018-11-01 15:35:24 +0300

    adds css and js  to  add parcel modal[finishes #161616205]

---
+++ UI/js/main.js
@@ -0,0 +1,8 @@
+var  parcel_order_modal = document.getElementById('order-parcel-modal');
+function display_add_parcel_order() {
+    parcel_order_modal.style.display = "block";
+}
+/* function to close the order parcel modal*/
+function close_modal() {
+    parcel_order_modal.style.display ="none";
+}
